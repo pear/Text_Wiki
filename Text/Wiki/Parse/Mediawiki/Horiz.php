1d25b1d3a4adeac567fb1ae52a373d3b01f2bc77
Daniel O'Connor <daniel.oconnor@gmail.com>
2012-01-02 21:29:42 +1030

    Missed files from commit

---
+++ Text/Wiki/Parse/Mediawiki/Horiz.php
@@ -0,0 +1,5 @@
+<?php
+require_once 'Text/Wiki/Parse/Default/Horiz.php';
+
+class Text_Wiki_Parse_Mediawiki_Horiz extends Text_Wiki_Parse_Default_Horiz 
+{}
---
+++ Text/Wiki/Parse/Mediawiki/Toc.php
@@ -0,0 +1,5 @@
+<?php
+require_once 'Text/Wiki/Parse/Default/Toc.php';
+
+class Text_Wiki_Parse_Mediawiki_Toc extends Text_Wiki_Parse_Default_Toc 
+{}
