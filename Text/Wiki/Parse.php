c1f9c1174ac0650264bf3994e70d6f194a65eade
Daniel O'Connor <daniel.oconnor@gmail.com>
2012-01-02 21:43:38 +1030

    Fix parse behaviour

--- Text/Wiki/Parse.php
+++ Text/Wiki/Parse.php
@@ -117,7 +117,7 @@ class Text_Wiki_Parse {
         // to the tokens array. strip off the Text_Wiki_Parse_ portion.
         // text_wiki_parse_
         // 0123456789012345
-        $tmp = substr(get_class($this), 16);
+        list(,,,$tmp) = explode("_", get_class($this);
         $this->rule = ucwords(strtolower($tmp));
 
         // override config options for the rule if specified
