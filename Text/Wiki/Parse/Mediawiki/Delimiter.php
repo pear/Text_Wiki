cac68174bb0094ab5a9cfc06a6851d4178324b4b
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-06-05 12:00:19 +0930

    Split to own repo

--- Text/Wiki/Parse/Mediawiki/Delimiter.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'Text/Wiki/Parse/Default/Delimiter.php';
-
-class Text_Wiki_Parse_Mediawiki_Delimiter extends Text_Wiki_Parse_Default_Delimiter 
-{}
--- Text/Wiki/Parse/Mediawiki/Horiz.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'Text/Wiki/Parse/Default/Horiz.php';
-
-class Text_Wiki_Parse_Mediawiki_Horiz extends Text_Wiki_Parse_Default_Horiz 
-{}
